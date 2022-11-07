08dab27040e4d2caf84b0c4bf296a76aa79fc99a
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-11-07 16:21:45 +0100

    Aula 2

--- 1-JavaScript-para-iniciantes/2-tipos-de-dados/script.js
+++ 1-JavaScript-para-iniciantes/2-tipos-de-dados/script.js
@@ -1,13 +1,27 @@
-var nome = "Domingos"
-var apelido = "Canhanga"
+var nome = "Shinoda"
+var apelido = "Aoshi"
 var nomeCompleto = nome +' '+ apelido
 var idade = 26
  console.log(nomeCompleto, idade)
  // Template string
- var frase = `Domingos Canhanga idade ${idade}` 
+ var frase = `Moll Dany idade ${idade}` 
  console.log(frase)
- console.log(frase)
-
  /* Exercícios */
+// Declare uma variável contendo uma string
+var mySchool = 'INSUTEC'
+// Declare uma variável contendo um número dentro de uma string
+var numberInString = '2022'
+// Declare uma variável com a sua idade
+var myAge = 26
+// Declare duas variáveis, uma com seu nome
+// e outra com seu sobrenome e some as mesmas
+var myFirstName = 'Domingos'
+var myLastName = 'Canhanga'
+var fullName = myFirstName + ' ' + myLastName
+console.log(fullName)  
+// Coloque a seguinte frase em uma variável: It's time
+var Frase = "It's time"
+// Verifique o tipo da variável que contém o seu nome
+console.log(typeof myFirstName)
 
  
