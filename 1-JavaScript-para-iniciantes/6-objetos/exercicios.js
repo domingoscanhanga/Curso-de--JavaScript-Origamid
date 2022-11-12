4e8ba0657e2a7c7de2bed9deca1f4a67a413eed5
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-11-12 19:26:04 +0100

    Exercícios da Aula 6 resolvido

--- 1-JavaScript-para-iniciantes/6-objetos/exercicios.js
+++ 1-JavaScript-para-iniciantes/6-objetos/exercicios.js
@@ -2,6 +2,33 @@
 // Deve possuir pelo menos duas propriedades nome e sobrenome
 var dadosPessoais = {
     nome: "Domingos",
-    apelido: "Canhanga"
+    apelido: "Canhanga",
 }
-console.log('ola');
+// Crie um método no objeto anterior, que mostre o seu nome completo
+dadosPessoais.nomeCompleto = function () {
+   return `${this.nome} ${this.apelido}`;
+}
+// Modifique o valor da propriedade preco para 3000
+var carro = {
+    preco: 1000,
+    portas: 4,
+    marca: 'Audi',
+  }
+  carro.preco = 3000;
+// Crie um objeto de um cachorro que represente um labrador,
+// preto com 10 anos, que late ao ver um homem
+var cachorro = {
+    raca: "labrador",
+    idade: 10,
+    cor :"preto",
+    
+    latir (pessoa) {
+        if (pessoa === "homem") {
+            return "late";
+        }
+        else {
+            return "Não late"
+        }
+    }
+}
+console.log(cachorro.latir("homem"))
