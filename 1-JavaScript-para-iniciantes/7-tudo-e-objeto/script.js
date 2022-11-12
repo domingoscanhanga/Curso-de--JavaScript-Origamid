217bc4a78f087fd795945ce2144bcfccaca4470e
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-11-12 22:37:04 +0100

    Aula 7

---
+++ 1-JavaScript-para-iniciantes/7-tudo-e-objeto/script.js
@@ -0,0 +1,11 @@
+/*
+Tudo é Objeto
+Strings, Números, Boolean, Objetos e mais possuem propriedades e métodos. Por isso são objetos.
+*/
+var nome ="Pano";
+nome.length; // length propriedade que retorna total de carácter de uma string
+nome.charAt(2); // Método que retorna o carácter nessa posição.
+nome.replace('Pa', 'la');1
+
+
+
