540a2651c22f9990a32e37bfcf3c5e3d22290db2
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-11-26 10:42:57 +0100

    Chapter 2, lesson 2

---
+++ 02 JavaScript Para Iniciantes/0202-tipos-de-dados/script-aula.js
@@ -0,0 +1,22 @@
+let $name = "Domingos";
+console.log(typeof $name);
+let $age = 27;
+console.log(typeof $age);
+let $isHuman = true;
+console.log(typeof $isHuman);
+let $time;
+console.log(typeof $time);
+let $food = null;
+console.log(typeof $food);
+let $simbolo = Symbol() // Symbol
+console.log(typeof $simbolo);
+let $novObjeto = {} // Object
+console.log(typeof $novObjeto);
+let array = [];
+console.log(typeof array);
+let $firstName = "Domingos";
+let $lastName = "Canhanga"
+let $fullName = $firstName + " "+$lastName;
+console.log($fullName);
+let $phrase = "O "+ $fullName + " tem " + $age;
+console.log($phrase);
