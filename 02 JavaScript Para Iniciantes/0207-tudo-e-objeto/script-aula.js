db1c186b35f9cc24c0c65f8b1002c354d6425ab6
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-12-04 11:11:34 +0100

    Chapter 2, Lesson 7 Tudo é objeto

---
+++ 02 JavaScript Para Iniciantes/0207-tudo-e-objeto/script-aula.js
@@ -0,0 +1,17 @@
+const nome = 'Domingos';
+console.log(nome.length);
+const fruta = ['banana', 'pao'];
+console.log(fruta.length);
+console.log(nome.charAt(2));
+console.log(nome.replace('go','ga'));
+const idade = 26;
+const nota = 19.5;
+console.log(idade.toString());
+console.log(nota.toFixed());
+function somar(n1, n2, n3, n4) {
+  return n1 + n2 + n3 + n4;
+}
+console.log(somar.length);
+
+
+
