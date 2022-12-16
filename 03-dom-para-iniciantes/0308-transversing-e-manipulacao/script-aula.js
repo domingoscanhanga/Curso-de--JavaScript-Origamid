d32d862b89346d4ff18bf09f9a4664735b11890f
Domingos Canhanga <76398540+domingoscanhanga@users.noreply.github.com>
2022-12-16 13:05:08 +0100

    Chapter 3, Lesson 8 - Transversing e manipulação

---
+++ 03-dom-para-iniciantes/0308-transversing-e-manipulacao/script-aula.js
@@ -0,0 +1,41 @@
+/*
+const h1 = document.querySelector('h1');
+console.log(h1.innerHTML);
+console.log(h1.outerHTML);
+console.log(h1.innerText);
+console.log(h1.outerText);
+//h1.innerHTML='<p>Teste</p>'
+//h1.outerHTML='<p>Teste</p>'
+//h1.innerText = '<p>Teste</p>';
+//h1.outerText= '<p>Teste</p>'
+
+const lista = document.querySelector('.animais-lista');
+// Elementos a cima do elemento mencionado 
+const pai = lista.parentElement; 
+console.log(pai);
+const paiDoPai = pai.parentElement;
+console.log(paiDoPai);
+ // Elementos dentro do elemento mencionado
+filhoPai = pai.children;
+filhoLista = lista.children;
+console.log(filhoLista[0]);
+console.log(filhoPai[0]);
+const contato = document.querySelector('.contato');
+const titulo =contato.querySelector('.titulo');
+//contato.appendChild(lista);
+contato.insertBefore(lista, titulo)  // insere a lista antes de titulo
+ //contato.removeChild(titulo); // remove titulo de contato
+//contato.replaceChild(lista, titulo); // substitui titulo por lista
+const animal = document.querySelector('.animais');
+const novoH1 = document.createElement('h1');
+novoH1.innerHTML = 'Novo titulo';
+novoH1.classList.add('titulo');
+animal.appendChild(novoH1);
+
+const tituloClone = titulo.cloneNode(true);
+lista.appendChild(tituloClone);
+*/
+
+
+
+
